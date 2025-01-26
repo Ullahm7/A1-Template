1bf2216759def1a59c650546001e65cf99dcd8c2
Muhammad Ullah <muhullah25@gmail.com>
2025-01-25 20:11:05 -0800

    Possible positioning and out-of-bound conditions

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Position.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Position.java
@@ -1,26 +1,68 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 public class Position {
+
     private int x;
     private int y;
     private Direction direction;
+    private Maze maze;
 
-    public Position(int x, int y, Direction direction) {
+    public Position(int x, int y, Direction direction, Maze maze) {
+
         this.x = x;
         this.y = y;
         this.direction = direction;
+        this.maze = maze;
+
     }
 
-    //getters, setters, and methods for moving and turning
     public void moveForward() {
-        //modify x and y based on the current direction
+
+        int newX = x;
+        int newY = y;
+
+        //move based on current direction
+        switch (direction.getCurrentDirection()) {
+            case "NORTH":
+                newX--; //move up
+                break;
+            case "EAST":
+                newY++; //move right
+                break;
+            case "SOUTH":
+                newX++; //move down
+                break;
+            case "WEST":
+                newY--; //move left
+                break;
+        }
+
+        //validate the new position
+        if (newX >= 0 && newY >= 0 && newX < maze.getRows() && newY < maze.getCols()) {
+            if (maze.getTile(newX, newY) == ' ') { 
+                this.x = newX;
+                this.y = newY;
+            } else {
+                System.out.println("Hit a wall! Can't move forward."); //out-of-bound conditions
+            }
+        } else {
+            System.out.println("Out of bounds! Can't move forward."); //out-of-bound conditions
+        }
     }
 
     public void turnLeft() {
-        //change to the left
+        direction.turnLeft();
     }
 
     public void turnRight() {
-        //change to the right
+        direction.turnRight();
+    }
+
+    public String getCurrentPosition() {
+        return "(" + x + ", " + y + ") facing " + direction.getCurrentDirection();
+    }
+
+    public int[] getCoordinates() {
+        return new int[]{x, y};
     }
 }
